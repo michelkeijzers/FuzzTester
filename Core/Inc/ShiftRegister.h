cf1bd738c9a1fec5c4a879fac4035575ac1e5090
Michel Keijzers <michelkeijzers@hotmail.com>
2020-01-30 22:54:36 +0100

    Add files via upload

---
+++ Core/Inc/ShiftRegister.h
@@ -0,0 +1,32 @@
+/*
+ * ShiftRegister.h
+ *
+ *  Created on: Jan 30, 2020
+ *      Author: miche
+ */
+
+#ifndef SHIFTREGISTER_H_
+#define SHIFTREGISTER_H_
+
+//#include "stm32f1xx_hal_def.h"
+
+#include "stm32f1xx_hal.h"
+//#include "stm32f1xx_hal_spi.h"
+//#include "stm32f103xb.h"
+
+class ShiftRegister
+{
+public:
+	ShiftRegister(SPI_HandleTypeDef* hSpi, GPIO_TypeDef* latchPort, uint16_t latchPin);
+	virtual ~ShiftRegister();
+
+	void ShiftOut(uint8_t* data, uint16_t size);
+
+private:
+	SPI_HandleTypeDef* _hSpi;
+
+	GPIO_TypeDef*      _latchPort;
+	uint16_t           _latchPin;
+};
+
+#endif /* SHIFTREGISTER_H_ */
